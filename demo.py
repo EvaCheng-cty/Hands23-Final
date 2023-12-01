8b2b45a2e86497d51cb8ef7b5830506e106e0bc3
Eva Cheng <evacheng@umich.edu>
2023-12-01 13:48:23 -0500

    vis on sam

--- demo.py
+++ demo.py
@@ -320,14 +320,14 @@ def main():
     f = open("/w/fouhey/hands2/allMerged7Splits/VAL.txt")
     images = f.readlines()
 
-    random.seed(42)
-    random.shuffle(images)
+    # random.seed(42)
+    # random.shuffle(images)
 
-    images = []
-    images.append( "AR_5PIGFuqoJnY_3_1800_55.jpg")
+    # images = []
+    # images.append( "AR_5PIGFuqoJnY_3_1800_55.jpg")
 
     # outputs
-    save_dir = "/launch/evacheng/vis_Nov/val_test/"
+    save_dir = "/launch/evacheng/vis_Nov/val/"
     save_mask_dir = f"{save_dir}/masks" 
     os.makedirs(save_dir, exist_ok=True)
     os.makedirs(save_mask_dir, exist_ok=True)
